09304e20599807b3611420bbd92218c48a24c4f0
htgithub99 <65000988+htgithub99@users.noreply.github.com>
2020-08-19 19:49:14 +0700

    Update thamgia.php

--- thamgia.php
+++ thamgia.php
@@ -110,8 +110,8 @@ function ketnoi($userid,$gioitinh) { //tìm người chát
 } else {  // neu co nguoi trong hàng chờ
     addketnoi($userid, $partner);
  
- sendchat($partner,"✅ Bạn đã được kết nối với một người lạ(👤)");  
- sendchat($userid,"✅ Bạn đã được kết nối với một người lạ(👤)");  
+ sendchat($partner,"✅ Bạn đã được kết nối với một người lạ(👤) - Gõ End để kết thúc trò chuyện");  
+ sendchat($userid,"✅ Bạn đã được kết nối với một người lạ(👤) - Gõ End để kết thúc trò chuyện");  
   
   }
 }
